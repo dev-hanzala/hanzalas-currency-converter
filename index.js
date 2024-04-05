c6f26fd34094a884c5dd9f36d3a815c72d309dde
dev-hanzala <160335647+dev-hanzala@users.noreply.github.com>
2024-04-06 00:50:37 +0500

    v1.0.0

--- index.js
+++ index.js
@@ -1,2 +1,45 @@
-#! /usr/env/bin/ node
-export {};
+import inquirer from "inquirer";
+
+let exchangeRates = {
+  USD: 1,
+  EUR: 0.9,
+  GBP: 0.75,
+  PKR: 280,
+  INR: 70
+};
+
+let input = await inquirer.prompt([
+  {
+    name: "fromCurrency",
+    type: "list",
+    choices: [
+      "USD",
+      "EUR",
+      "GBP",
+      "PKR",
+      "INR",
+    ],
+    message: "From currency:"
+  },
+  {
+    name: "toCurrency",
+    type: "list",
+    choices: [
+      "USD",
+      "EUR",
+      "GBP",
+      "PKR",
+      "INR",
+    ],
+    message: "To currency:"
+  },
+  {
+    name: "amount",
+    type: "number",
+    message: "Amount:"
+  }
+]);
+
+let result = (input.amount / exchangeRates[input.fromCurrency])  * exchangeRates[input.toCurrency];
+
+console.log(`That will be ${result} ${input.toCurrency}`);
--- index.ts
+++ index.ts
@@ -1,3 +1,46 @@
 #! /usr/env/bin/ node
 
 import inquirer from "inquirer";
+
+let exchangeRates = {
+	USD: 1,
+	EUR: 0.9,
+	GBP: 0.75,
+	PKR: 280,
+	INR: 70
+};
+
+let input = await inquirer.prompt(
+	[
+		{
+			name: "fromCurrency",
+			type: "list",
+			choices: [
+      		"USD",
+      		"EUR",
+      		"GBP",
+     			"PKR",
+				"INR",
+			],
+			message: "From currency:"
+		},
+		{
+			name: "toCurrency",
+         type: "list",
+         choices: [
+            "USD",
+            "EUR",
+            "GBP",
+            "PKR",
+            "INR",
+         ],
+         message: "To currency:"
+		},
+		{
+         name: "amount",
+         type: "number",
+         message: "Amount:"
+      }
+	]
+);
+console.log(input);
